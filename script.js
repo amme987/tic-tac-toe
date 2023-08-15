5a093fcd02ef8a121c55022a1474f06e04e2be9a
amme987 <emmagination97@gmail.com>
2023-08-15 10:05:00 -0400

    Create and display gameboard

---
+++ script.js
@@ -0,0 +1,34 @@
+// Gameboard module
+const Gameboard = (() => {
+  const gameboard = [];
+  const rows = 3;
+  const columns = 3;
+
+  // Gameboard array
+  for (let i = 0; i < rows; i++) {
+    gameboard[i] = [];
+    for (let j = 0; j < columns; j++) {
+      (i + j) % 2 === 0 ? (gameboard[i][j] = "X") : (gameboard[i][j] = "O");
+    }
+  }
+
+  // Create a button element for each element in the array
+  const board = document.querySelector(".board");
+  const test = gameboard.forEach(row => {
+    row.forEach(item => {
+      const button = document.createElement("button");
+      button.textContent = item;
+      board.appendChild(button);
+    });
+  });
+
+  return { gameboard, test };
+})();
+
+console.log(Gameboard);
+
+// Player factory
+const Player = () => {};
+
+// displayController module
+const displayController = (() => {})();
